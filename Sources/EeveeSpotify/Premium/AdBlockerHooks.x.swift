9414d83a37b3fb6165a75fde1b1d0484799b9dfe
jaydenjcpy <jaydenjcpy3@gmail.com>
2026-03-31 15:16:41 +0800

    Update AdBlockerHooks.x.swift

--- Sources/EeveeSpotify/Premium/AdBlockerHooks.x.swift
+++ Sources/EeveeSpotify/Premium/AdBlockerHooks.x.swift
@@ -2,118 +2,141 @@ import Orion
 import Foundation
 import UIKit
 
-// MARK: - Ad View Controllers and Views
+// MARK: - Safe Ad View Controllers and Views Hooks
+// We use dynamic targetName and check if classes exist to prevent crashes on launch.
+// We also avoid hooking initWithFrame on Swift classes as it causes ObjC swizzling crashes.
 
 class MarqueeContentViewControllerHook: ClassHook<UIViewController> {
     typealias Group = BasePremiumPatchingGroup
-    static let targetName = "MarqueeContentViewController"
+    
+    static var targetName: String {
+        return NSClassFromString("MarqueeContentViewController") != nil ? "MarqueeContentViewController" : "UIViewController"
+    }
     
     func viewDidLoad() {
         orig.viewDidLoad()
-        target.view.isHidden = true
-        target.view.alpha = 0
-        target.view.frame = .zero
+        if String(describing: type(of: target)) == "MarqueeContentViewController" {
+            target.view.isHidden = true
+            target.view.alpha = 0
+            target.view.frame = .zero
+        }
     }
     
     func viewWillAppear(_ animated: Bool) {
         orig.viewWillAppear(animated)
-        target.dismiss(animated: false, completion: nil)
+        if String(describing: type(of: target)) == "MarqueeContentViewController" {
+            target.dismiss(animated: false, completion: nil)
+        }
     }
 }
 
 class MarqueeClassicContentViewControllerHook: ClassHook<UIViewController> {
     typealias Group = BasePremiumPatchingGroup
-    static let targetName = "MarqueeClassicContentViewController"
+    
+    static var targetName: String {
+        // Use the exact mangled name found in the binary, or fallback to UIViewController
+        let mangledName = "_TtC19Marquee_MarqueeImpl35MarqueeClassicContentViewController"
+        if NSClassFromString(mangledName) != nil {
+            return mangledName
+        } else if NSClassFromString("MarqueeClassicContentViewController") != nil {
+            return "MarqueeClassicContentViewController"
+        }
+        return "UIViewController"
+    }
     
     func viewDidLoad() {
         orig.viewDidLoad()
-        target.view.isHidden = true
-        target.view.alpha = 0
-        target.view.frame = .zero
+        let className = String(describing: type(of: target))
+        if className.contains("MarqueeClassicContentViewController") {
+            target.view.isHidden = true
+            target.view.alpha = 0
+            target.view.frame = .zero
+        }
     }
     
     func viewWillAppear(_ animated: Bool) {
         orig.viewWillAppear(animated)
-        target.dismiss(animated: false, completion: nil)
-    }
-}
-
-class LeaveBehindViewModelHook: ClassHook<NSObject> {
-    typealias Group = BasePremiumPatchingGroup
-    static let targetName = "LeaveBehindViewModel"
-    
-    func shouldShowLeaveBehind() -> Bool {
-        return false
-    }
-}
-
-class DisplayAdCardElementHook: ClassHook<UIView> {
-    typealias Group = BasePremiumPatchingGroup
-    static let targetName = "DisplayAdCardElement"
-    
-    func initWithFrame(_ frame: CGRect) -> UIView {
-        let view = orig.initWithFrame(.zero)
-        view.isHidden = true
-        view.alpha = 0
-        return view
-    }
-    
-    func layoutSubviews() {
-        orig.layoutSubviews()
-        target.isHidden = true
-        target.frame = .zero
-    }
-}
-
-class FullbleedDisplayAdElementUIHook: ClassHook<UIView> {
-    typealias Group = BasePremiumPatchingGroup
-    static let targetName = "AdsPlatform_ElementKit.FullbleedDisplayAdElementUI"
-    
-    func initWithFrame(_ frame: CGRect) -> UIView {
-        let view = orig.initWithFrame(.zero)
-        view.isHidden = true
-        view.alpha = 0
-        return view
-    }
-    
-    func layoutSubviews() {
-        orig.layoutSubviews()
-        target.isHidden = true
-        target.frame = .zero
-    }
-}
-
-class SPTBannerViewHook: ClassHook<UIView> {
-    typealias Group = BasePremiumPatchingGroup
-    static let targetName = "SPTBannerView"
-    
-    func initWithFrame(_ frame: CGRect) -> UIView {
-        let view = orig.initWithFrame(.zero)
-        view.isHidden = true
-        view.alpha = 0
-        return view
-    }
-    
-    func layoutSubviews() {
-        orig.layoutSubviews()
-        target.isHidden = true
-        target.frame = .zero
+        let className = String(describing: type(of: target))
+        if className.contains("MarqueeClassicContentViewController") {
+            target.dismiss(animated: false, completion: nil)
+        }
     }
 }
 
 class SPTBannerViewControllerHook: ClassHook<UIViewController> {
     typealias Group = BasePremiumPatchingGroup
-    static let targetName = "SPTBannerViewController"
+    
+    static var targetName: String {
+        return NSClassFromString("SPTBannerViewController") != nil ? "SPTBannerViewController" : "UIViewController"
+    }
     
     func viewDidLoad() {
         orig.viewDidLoad()
-        target.view.isHidden = true
-        target.view.alpha = 0
-        target.view.frame = .zero
+        if String(describing: type(of: target)) == "SPTBannerViewController" {
+            target.view.isHidden = true
+            target.view.alpha = 0
+            target.view.frame = .zero
+        }
     }
     
     func viewWillAppear(_ animated: Bool) {
         orig.viewWillAppear(animated)
-        target.dismiss(animated: false, completion: nil)
+        if String(describing: type(of: target)) == "SPTBannerViewController" {
+            target.dismiss(animated: false, completion: nil)
+        }
+    }
+}
+
+// Hooking layoutSubviews is safer than initWithFrame for Swift UIViews
+class SPTBannerViewHook: ClassHook<UIView> {
+    typealias Group = BasePremiumPatchingGroup
+    
+    static var targetName: String {
+        return NSClassFromString("SPTBannerView") != nil ? "SPTBannerView" : "UIView"
+    }
+    
+    func layoutSubviews() {
+        orig.layoutSubviews()
+        if String(describing: type(of: target)) == "SPTBannerView" {
+            target.isHidden = true
+            target.frame = .zero
+        }
+    }
+}
+
+// For Swift UI classes, we use the mangled names and only hook layoutSubviews
+class DisplayAdCardElementUIHook: ClassHook<UIView> {
+    typealias Group = BasePremiumPatchingGroup
+    
+    static var targetName: String {
+        let mangledName = "_TtC22AdsPlatform_ElementKit22DisplayAdCardElementUI"
+        return NSClassFromString(mangledName) != nil ? mangledName : "UIView"
+    }
+    
+    func layoutSubviews() {
+        orig.layoutSubviews()
+        let className = String(describing: type(of: target))
+        if className.contains("DisplayAdCardElementUI") {
+            target.isHidden = true
+            target.frame = .zero
+        }
+    }
+}
+
+class FullbleedDisplayAdElementUIHook: ClassHook<UIView> {
+    typealias Group = BasePremiumPatchingGroup
+    
+    static var targetName: String {
+        let mangledName = "_TtC22AdsPlatform_ElementKit27FullbleedDisplayAdElementUI"
+        return NSClassFromString(mangledName) != nil ? mangledName : "UIView"
+    }
+    
+    func layoutSubviews() {
+        orig.layoutSubviews()
+        let className = String(describing: type(of: target))
+        if className.contains("FullbleedDisplayAdElementUI") {
+            target.isHidden = true
+            target.frame = .zero
+        }
     }
 }
